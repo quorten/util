f4af08c8ba50350d39356042f428f64b776d95eb
Andrew Makousky <mako0042@umn.edu>
2011-05-24 01:42:30 -0500

    Save file.

---
+++ hidewin.h
@@ -0,0 +1,12 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#define MAINMENU	102
+#define MAINLBOX	103
+#define M_UPDATE	1001
+#define M_SHOW		1002
+#define M_HIDE		1003
+#define M_REVEAL	1004
+#define M_EXIT		1005
+
+#endif
