b05c1f9dc66bfa70ae463ca1d95115113400c4d8
Andrew Makousky <mako0042@umn.edu>
2013-01-03 20:44:46 -0600

    Save file.

---
+++ cplstub.h
@@ -0,0 +1,21 @@
+#ifndef CPLFUNC_H
+#define CPLFUNC_H
+
+#define WIN32_LEAN_AND_MEAN /* This might not be neccisary since it
+			       excludes things that we need to include */
+#include <windows.h>
+#include <cpl.h>
+#include <shellapi.h>
+
+#include "bool.h"
+
+HANDLE hInst = NULL;
+
+#define NUM_SUBPROGS 1
+
+bool bCallProgram[NUM_SUBPROGS] = {1}; /* either call external program
+					  or call internal function */
+typedef void(* CplFunction)(LPARAM);
+CplFunction ResponseFunc[NUM_SUBPROGS] = {NULL};
+
+#endif
