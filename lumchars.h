9eef5b3663e20d0ada267525110ca540e9edff33
Andrew Makousky <mako0042@umn.edu>
2013-01-13 07:38:30 -0600

    Save file.

---
+++ lumchars.h
@@ -0,0 +1,6 @@
+/* A list of ASCII characters that maps to luminance values.
+   Don't ask how I got this. */
+
+/* Arg... we have to leave room for the NULL character */
+const char ascChars[257] =
+"MMMNNNWWWBBRRRHHHKKKEEAAAQQQDD88899966000GGGOOOUUVVVPPPXX###@@@44&&&mmmwww$$SSS555ZZ222FFFggpppqqqyyybbdddkkkhh333CCCYYTTTuuunnnaaeeeooo**cccssszzvvvxxxJJJLL777\?\?\?!!%%%111ffjjjtttrrr{{}}}[[[]]((()))<<>>>IIIlllii\\\\\\///||===+++^^;;;:::\"\"\"--___~~~''',,,```...";
